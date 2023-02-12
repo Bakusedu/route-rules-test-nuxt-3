c71a09e3eb1ab55e14dd9bc6a06232a5e0715b65
Chiedu Emmanuel <iamnedu912@gmail.com>
2023-02-12 22:27:30 +0100

    Update time func to async api

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,12 @@
-<script setup></script>
+<script setup>
+const { data, pending, error } = await useFetch(
+  "https://worldtimeapi.org/api/ip"
+);
+</script>
 <template>
   <div>
     <h1>Static template</h1>
-    <h2>{{ new Date() }}</h2>
+    <h2>{{ data.datetime }}</h2>
+    <h2>{{ data.unixtime }}</h2>
   </div>
 </template>
